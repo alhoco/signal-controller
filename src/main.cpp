3300ff90cfce19fcbee2df8717d57b7d599fc546
Alejandro Hoyos Correa <ahoyosc30@gmail.com>
2021-02-11 19:14:39 -0500

    Se agrego los entornos de codigo a git new file:   include/README new file:   lib/README new file:   platformio.ini new file:   src/main.cpp new file:   test/README

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+
+void loop() {
+  int setpoint_signal = analogRead(A1);
+  int input_signal = analogRead(A0);
+}
