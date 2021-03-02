b79d70c5f8d316a4165a78d4710213050d8ad549
Alejandro Hoyos Correa <ahoyosc30@gmail.com>
2021-03-01 19:48:33 -0500

    Se agregaron las variables que determinan el estado anterior del compresor

--- src/Output/Output.h
+++ src/Output/Output.h
@@ -9,13 +9,20 @@ void setup() {
 }
 
 namespace output{
+    int compressor_state = 0;
+    int last_compressor_state = 0;
 
     void Output(float input, float setpoint){
+        
+        last_compressor_state = compressor_state;
+        
         if (input >= setpoint){
             digitalWrite(Compressor, HIGH);
+            compressor_state = 1;
         }
         else {
             digitalWrite(Compressor, LOW);
+            compressor_state = 0;
         }
     }
 }
