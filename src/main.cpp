48adf3bf451b370e5c9d04796b1c72e6e6e9ca6b
Alejandro Hoyos Correa <ahoyosc30@gmail.com>
2021-03-24 11:25:14 -0500

    Se agrega la lectura del sensor y la aplicación del buffer circular para sacar el valor del sensor

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,35 @@
 #include <Output/Output.h>
 #include <processors/aggregator.h>
 #include <Output/Alarm.h>
+#include <Sensors/analog_sensor.h>
+
+CircularBuffer<int> circularbuffer = CircularBuffer<int>(6);
+analogsensor sensor = analogsensor(0x7E, A0);
+analogsensor setpoint_signal = analogsensor(0x7E, A1);
 
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
 }
 
 
 void loop() {
-  Settings::initSettings();
 
-  int setpoint_signal = analogRead(A1);
-  int input_signal = analogRead(A0);
-  float processed_signal = OffsetTransformer::transform(input_signal);
-  output::Output(processed_signal, setpoint_signal);
+Settings::initSettings();
+
+sensor.execute();
+setpoint_signal.execute();
+
+if (sensor.hasChanged()){
+  int raw_read = sensor.getValue().getValue();
+  circularbuffer.append(raw_read);
+  int temperature = circularbuffer.mean();
+}
+
+  float processed_signal = OffsetTransformer::transform(temperature);
+  
+  output::Output(processed_signal, setpoint_signal.getValue().getValue());
   int aggregated_signal = Aggregator::aggregator();
   Alarm::alarm(aggregated_signal);
 
