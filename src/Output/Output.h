09b7c6426069aa464005b25ee78e7495fbdb10b6
Alejandro Hoyos Correa <ahoyosc30@gmail.com>
2021-03-29 22:33:27 -0500

    Se cambia la forma de escritura del código

--- src/Output/Output.h
+++ src/Output/Output.h
@@ -3,7 +3,11 @@
 #define Compressor 2
 #define Amperage A1
 
-#include <Arduino.h>
+#include <arduino.h>
+#include <Sensors/analog_sensor.h>
+#include <settings.h>
+
+AnalogSensor Amp_Sensor = AnalogSensor(0x7E, A2);
 
 void setup() {
     pinMode(Compressor, LOW);
@@ -13,28 +17,41 @@ namespace output{
     int compressor_state = 0;
     int last_compressor_state = 0;
 
-    void Output(float input, float setpoint){
-        
-        if (input >= setpoint){
-            digitalWrite(Compressor, HIGH);
-            compressor_state = 1;
-        }
-        else {
-            digitalWrite(Compressor, LOW);
-            compressor_state = 0;
-        }
-        delay(3000);
+template<class T>
+class Outputrole{
+public:
+    void Output(uint8_t input_signal, uint8_t setpoint);
+private:
+    uint8_t last_compressor_state;
+    uint8_t compressor_state;
+};
 
-        float compressor_amperage = analogRead(A1);
-        if (compressor_amperage < 0){
-            last_compressor_state = compressor_state;
-            compressor_state = 0;
-        }
-        else {
-            last_compressor_state = compressor_state;
-        }
-        
+template<class T>
+void Outputrole<T>::Output(uint8_t input_signal, uint8_t setpoint){
+        // Aqui voy
+
+     if (input >= setpoint){
+        digitalWrite(Compressor, HIGH);
+        compressor_state = 1;
     }
+    else {
+        digitalWrite(Compressor, LOW);
+        compressor_state = 0;
+    }
+    delay(3000);
+
+    ampsensor.execute();
+    circularbufferamp.append(ampsensor);
+
+    if (ampsensor.getValue().getValue() < 0){
+        last_compressor_state = compressor_state;
+        compressor_state = 0;
+    }
+    else {
+        last_compressor_state = compressor_state;
+    }
+        
+}
 }
 
 #endif
