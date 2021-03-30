5446a9848011e7f2e56d9ceecb6e90bc989dfede
Alejandro Hoyos Correa <ahoyosc30@gmail.com>
2021-03-30 00:12:52 -0500

    Se crea la instancia que define y mapea un sensor de apmeraje

--- src/Sensors/sensor_amperaje.h
+++ src/Sensors/sensor_amperaje.h
@@ -4,8 +4,9 @@
 #include <Arduino.h>
 #include <Sensors/analog_sensor.h>
 
-class AmperajeSensor : public AnalogSensor {
-    AmperajeSensor(uint8_t id, uint8_t pin):AnalogSensor(id, pin, 0.01466){}
+class AmperageSensor : public AnalogSensor {
+    public:
+        AmperageSensor(uint8_t id, uint8_t pin):AnalogSensor(id, pin, 0.01466){}
 };
 
 #endif 
