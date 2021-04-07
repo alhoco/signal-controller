18a1505c35d620794bf2740109203d9abd2bd003
Alejandro Hoyos Correa <ahoyosc30@gmail.com>
2021-04-06 22:58:32 -0500

    Sea agrega funcion de desicion booleana en tansformador

--- src/processors/transformer.h
+++ src/processors/transformer.h
@@ -2,10 +2,24 @@
 #define TRANSFORMER_H
 
 #include <Arduino.h>
+#include <entities/value.h>
 
 template<class T>
 T transform(T input, T max_temperature, T max_sensor){
     return (input * max_temperature) / max_sensor;
+}
+
+template<class T>
+bool desitions(T value, T setPoint){
+    bool desition;
+    
+    if (value > setPoint){
+        desition = true;
+    }
+    else{
+        desition = false;
+    }
+    return desition; 
 }
 
 #endif
