7b18df424892e1e072ccced889123e9c11ba17c7
Alejandro Hoyos Correa <ahoyosc30@gmail.com>
2021-04-07 20:57:58 -0500

    Se cambia el tipo de variable que recibe

--- src/Actuators/Led.h
+++ src/Actuators/Led.h
@@ -3,20 +3,20 @@
 
 #include <Arduino.h>
 #include <Actuators/Actuators.h>
-#include <entities/signal.h>
+#include <entities/State.h>
 
-class Led : public ActuatorABS<Signal> {
+class Led : public ActuatorABS<State> {
     public:
         Led(uint8_t id, uint8_t pin):ActuatorABS(id){
             this->pin = pin;
         }
     private:
         uint8_t pin;
-        void write(Signal value);
+        void write(State value);
 };
 
-void Led::write(Signal value){
-    bool bool_value = 0.5 > value.getValue();   // Revisar voltaje/ampreaje de entrada
+void Led::write(State value){
+    bool bool_value = 0.5 < value.getValue();   // Revisar voltaje/ampreaje de entrada
     digitalWrite(this->pin, bool_value);
 }
 
